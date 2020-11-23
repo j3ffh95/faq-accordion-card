91707dd89ce49cde6182003974fb4bede86cb69b
j3ffh95 <j3ffh95@gmail.com>
2020-11-23 17:54:28 -0500

    Fixed a bug in the js logic

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let accordionsElements = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordionsElements.length; i++) {
   accordionsElements[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    // console.log(this.querySelector("img"));
+    this.querySelector("img").classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
