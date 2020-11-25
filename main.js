dddd0138c280ca72b24311290bf4b2df50b3a882
j3ffh95 <j3ffh95@gmail.com>
2020-11-25 15:12:41 -0500

    change the text animation when clicked

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ for (let i = 0; i < accordionsElements.length; i++) {
   accordionsElements[i].addEventListener("click", function () {
     // console.log(this.querySelector("img"));
     this.querySelector("img").classList.toggle("active");
+    this.classList.toggle("text-active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
